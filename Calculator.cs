08b61d14a32fb5f80d509f666d18f527cf2466d7
Gnd10 <60834447+Gnd10@users.noreply.github.com>
2026-08-09 10:28:50 +0700

    add new program

---
+++ Calculator.cs
@@ -0,0 +1,22 @@
+namespace MyApp
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }       
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+         public int Multiplie(int a, int b)
+        {
+            return a * b;
+        }
+        public double Divide(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using MyApp;
+
+class Program
+{
+    static void Main()
+    {
+        var calc = new Calculator();
+        int result = calc.Add(8, 5);
+        float result2 = calc.Subtract(10, 15);
+        int result3 = calc.Multiplie(3, 15);
+        double result4 = calc.Divide(6.5, 10.25);
+        Console.WriteLine($"Hasil: {result}");
+        Console.WriteLine($"Hasil: {result2}");
+        Console.WriteLine($"Hasil: {result3}");
+        Console.WriteLine($"Hasil: {result4}");
+    }
+}
